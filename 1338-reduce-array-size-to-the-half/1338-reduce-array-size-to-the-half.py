cde7ab931659f8c833ba38b26d9713cfae4d96e4
Robera16 <71070205+Robera16@users.noreply.github.com>
2022-04-25 13:14:15 +0300

    Time: 1265 ms (6.50%), Space: 36.2 MB (36.35%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,7 +1,6 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
         arr.sort()
-        
         dct={}
         stack=[]
         stack.append(arr[0])
@@ -15,17 +14,13 @@ class Solution:
                 count=1
                 stack.append(arr[val])
             val+=1
-            
         dct[stack[-1]]=count
-        
         dct=sorted(dct.items(),key= lambda x:x[1], reverse=True)
-        
         val=dct[0][1]
         output=1
         while(val < len(arr)//2):
             val+=dct[output][1]
             output+=1
-            
         return output
 
             
