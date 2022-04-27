14d0594bd70790463837474f1af20074e44c063c
Robera16 <71070205+Robera16@users.noreply.github.com>
2022-04-27 15:34:29 +0300

    Time: 1431 ms (31.37%), Space: 36.6 MB (38.43%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.py
@@ -0,0 +1,42 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        # position                         0, 3, 5, 8, 10
+        # speed                            1, 3, 1, 4, 2
+        # target(12)-position / speed      12,3, 7, 1, 1    hours to reach destination   
+        # output                           12, (3,7), (1,1) 
+        # stack content         12 then 12, 3 then 12,7 then 12,7,1 then 12, 7, 1 so len=3
+        # required HR for car at position 0 never be less than the rest if less
+        # add 1 to the fleet
+        
+        # 0,  2,  4
+        # 4,  2,  1
+        # 25, 49, 96
+        # (25, 49, 96)
+        # stack content 25 then 49 then 96 so len=1
+        
+        pws=[]
+        n=len(position)
+        for x in range(n):
+            pws.append((position[x], speed[x]))
+        
+        pws.sort()
+        Hrs=[]
+        for x in range(n):
+            Hrs.append((target-pws[x][0])/pws[x][1])
+        
+        stack=[]
+        for x in Hrs:
+            if len(stack):
+                if x >= stack[-1]:
+                    while(len(stack) and x >= stack[-1]):
+                        stack.pop()
+                    stack.append(x)
+                else:
+                    stack.append(x)
+            else:
+                stack.append(x)
+         
+        return len(stack)
+    
+        
