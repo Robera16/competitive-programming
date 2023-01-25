10a38a099a59bf8d16723848d60ef20c1bbe44fe
Robera16 <roberaworknehrobwork@gmail.com>
2023-01-25 20:49:41 +0300

    apply operation

---
+++ 2460. Apply Operations to an Array/Apply_Operations_to_an_Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+
+        write, read = 0, 0
+        while read < len(nums):
+            if nums[read] != 0:
+                nums[read], nums[write] = nums[write], nums[read]
+                write = write + 1
+            read = read + 1
+        
+        return nums
