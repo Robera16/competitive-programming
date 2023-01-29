61fd01e4e0c35073f48b3cf25c56e26c72775b2a
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-01-29 13:56:03 +0300

    Time: 201 ms (73.32%), Space: 15.4 MB (90.62%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -2,6 +2,7 @@ class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         """
             A triangle is valid if sum of any of its two sides is greater than the third side
+            (a + b <= c) or (a + c <= b) or (b + c <= a) return false
         """
         nums.sort()
         for i in range(len(nums)-1, 1, -1):
