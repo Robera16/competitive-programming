4a132b38c074dbee573f8987aedefe691ccf9938
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-02-01 22:38:51 +0300

    Time: 43 ms (63.36%), Space: 13.9 MB (72.67%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -2,9 +2,13 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         output=[]
         count=len(arr2)
+        dct={}
+        for index, val in enumerate(arr2):
+            dct[val] = index
+            
         for value in arr1:
-            if value in arr2:
-                output.append([value, arr2.index(value)])
+            if value in dct:  
+                output.append([value, dct[value]])
             else:
                 output.append([value, count+value])
                 
