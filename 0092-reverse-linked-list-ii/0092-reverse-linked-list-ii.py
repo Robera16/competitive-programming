5f8041528944e69492aa1c91c21e17e8a3282960
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-02-26 12:28:45 +0300

    Time: 29 ms (87.37%), Space: 14.1 MB (44.16%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -1,23 +1,40 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
+#     def init(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        dummy.next = head
+        leftNode, rightNode = None, None
+        # traverse and find left and right node
+        temp = head
+        val = 1
+        while temp:
+            if val==left:
+                leftNode = temp
+            if val==right:
+                rightNode = temp
+            val+=1
+            temp = temp.next
         
-        prev = dummy
+        prv = rightNode.next
+        val = (right - left) + 1
+        while val:
+            temp = leftNode.next
+            leftNode.next = prv
+            prv = leftNode
+            leftNode = temp
+            val-=1
        
-        for i in range(left - 1):
-            prev = prev.next
-        start = prev.next
-        end = start.next
-       
-        for i in range(right - left):
-            start.next = end.next
-            end.next = prev.next
-            prev.next = end
-            end = start.next
-        return dummy.next
+        leftt = head
+        tempLeft = left
+        while left-2 > 0:
+            leftt = leftt.next
+            left-=1
+            
+        
+        if tempLeft > 1:
+            leftt.next = rightNode
+            return head
+        else:
+            return  rightNode
