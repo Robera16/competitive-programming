2fb67e3b81eeb7baa45b72c0eccb97f8637618dd
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-03-06 22:06:50 +0300

    Time: 102 ms (89.20%), Space: 15.2 MB (73.86%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        start=0
+        sHeap=[0]*26
+        pHeap=[0]*26
+        k=len(p)
+        output=[]
+        for c in p:
+            pHeap[ord(c)-ord('a')]+=1
+            
+        for i in range(len(s)):
+            sHeap[ord(s[i])-ord('a')]+=1
+            if ((i-start+1)==k):
+                if pHeap==sHeap:
+                    output.append(start)
+                    
+                sHeap[ord(s[start])-ord('a')]-=1
+                start+=1
+                
+        return output
+        
