2dbfc2d34aa51ff0b57a33be6e50653e4ad8d9fc
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-04-11 23:15:10 +0300

    Time: 82 ms (38.26%), Space: 17.2 MB (63.55%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        # Convert time points to minutes since midnight and sort the list
+        minutes = sorted([int(time[:2]) * 60 + int(time[3:]) for time in timePoints])
+        
+        # Calculate the minimum difference between adjacent time points
+        min_diff = float('inf')
+        for i in range(len(minutes) - 1):
+            diff = minutes[i+1] - minutes[i]
+            if diff < min_diff:
+                min_diff = diff
+        
+        # Calculate the difference between the first and last time points
+        diff = (24*60 - minutes[-1] + minutes[0]) % (24*60)
+        if diff < min_diff:
+            min_diff = diff
+        
+        return min_diff
