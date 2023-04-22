837aa7c4e732e8e150afb9060b1464d09af63446
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-04-23 00:13:17 +0300

    Time: 31 ms (70.60%), Space: 13.8 MB (96.87%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,30 +1,25 @@
-from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """    
-        def check(p, q):
-            # if both are None
-            if not p and not q:
-                return True
-            # one of p and q is None
-            if not q or not p:
-                return False
-            if p.val != q.val:
-                return False
-            return True
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        output_p = []
+        output_q = []
         
-        deq = deque([(p, q),])
-        while deq:
-            p, q = deq.popleft()
-            if not check(p, q):
-                return False
+        def inorderTraversal(root, output,c):
+            if not root:
+                return
+            inorderTraversal(root.left, output,c+1)
+            output.append([root.val,c])
+            inorderTraversal(root.right, output,c+1)
             
-            if p:
-                deq.append((p.left, q.left))
-                deq.append((p.right, q.right))
-                    
-        return True
+        inorderTraversal(p, output_p,0)
+        inorderTraversal(q, output_q,0)
+        
+        print(output_p)
+        print(output_q)
+        return output_p==output_q
+        
