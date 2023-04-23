638896cd00ae957690f95c99407e7568935e5bd5
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-04-23 13:07:41 +0300

    Time: 1151 ms (5.94%), Space: 14.1 MB (92.73%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        output=[]
+        self.dfs(nums, [], output)
+        return output
+    
+    def dfs(self, nums, path, output):
+        if not nums and path not in output:
+            output.append(path)
+        
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], output)
+        
