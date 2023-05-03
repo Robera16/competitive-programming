d580aad7c9d0815278647c13b4b6fd18a3619ff7
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-05-03 23:55:52 +0300

    Time: 964 ms (31.37%), Space: 28.5 MB (10.72%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.py
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.py
@@ -3,20 +3,11 @@ class Solution:
         n=len(nums)
         prime, output= False, 0
         
-        def is_prime(x: int)->bool:
-            d=2
-            while d*d<=x:
-                if x%d==0:
-                    return False
-                d+=1
-            return True
-        
-        
         for i in range(n):
-            if is_prime(nums[i][i]):
+            if self.is_prime(nums[i][i]):
                 output=max(output, nums[i][i])
                 prime=True
-            if is_prime(nums[i][n-1-i]):  
+            if self.is_prime(nums[i][n-1-i]):  
                 output=max(output, nums[i][n-1-i])
                 prime=True
         
@@ -24,4 +15,10 @@ class Solution:
             return output
         return 0
         
-        
+    def is_prime(self, x: int)->bool:
+        d=2
+        while d*d<=x:
+            if x%d==0:
+                return False
+            d+=1
+        return True
