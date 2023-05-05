d43d8b538a2025f41a238efc1c3014a6794171e9
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-05-05 21:22:38 +0300

    Time: 75 ms (40.92%), Space: 18.4 MB (5.57%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,4 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return int(round((sum(set(nums)) - sum(nums)/3)*1.5))
-        
+        # explantion
+        #[x,x,x,y,z,z,z] => [3x,3z,y]
+        # uniques [x, z, y]
+        # x+z+y - (3x+3z+y)/3
+        # y -y/3 => 2y/3
+        # multiply the number by 2/3(1.5) to find y
+        # to assume the value of any fracton of numbers left during division use rounding
