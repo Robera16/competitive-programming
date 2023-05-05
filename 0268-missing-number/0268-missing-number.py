e2618593f6cdfea37734a262cc5ccb05377fce05
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-05-05 15:44:03 +0300

    Time: 144 ms (36.93%), Space: 17.7 MB (8.70%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,4 +1,20 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        result = 0
         n = len(nums)
-        return (n*(n+1))//2 - sum(nums)
+        for i in range(n+1): # i-> 0,1,2,3
+            result ^= i  # 0^0^1^2^3 result = 2
+        
+        for val in nums:
+            result ^= val # (0^0^1^2^3) ^ (3^0^1)
+        
+        # applying commutative property
+        # (0^0^1^2^3) ^ (3^0^1)
+        # (0^0^0)^(1^1)^(2)^(3^3)
+        # 0^0^2^0 
+        # 2
+        return result
+        
+        # Gaussian formula
+        # n = len(nums)
+        # return (n*(n+1))//2 - sum(nums)
