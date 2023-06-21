ec829cef5e4e732e8aacdf8515bd095a7940759f
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-06-21 21:15:59 +0300

    Time: 115 ms (67.82%), Space: 17.8 MB (86.34%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,31 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-            from collections import defaultdict
+           
             from queue import Queue
             def topological_sort(graph):
-                # Step 1: Calculate in-degree
+                
                 in_degree = [0] * len(graph)
                 for node in range(len(graph)):
                     for neighbor in graph[node]:
                         in_degree[neighbor] += 1
 
-                # Step 2: Enqueue nodes with in-degree of zero
-                queue = Queue()
+                queue = deque()
                 for node in range(len(graph)):
                     if in_degree[node] == 0:
-                        queue.put(node)
+                        queue.append(node)
 
-                # Step 3: Perform topological sorting
                 result = []
-                while not queue.empty():
-                    node = queue.get()
+                while len(queue):
+                    node = queue.popleft()
                     result.append(node)
                     for neighbor in graph[node]:
                         in_degree[neighbor] -= 1
                         if in_degree[neighbor] == 0:
-                            queue.put(neighbor)
+                            queue.append(neighbor)
 
-                # Step 4: Check for cycles
+               
                 if len(result) != len(graph):
                     return []
 
