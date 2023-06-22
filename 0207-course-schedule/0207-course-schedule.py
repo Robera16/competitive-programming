0075e53a0e23c26e886f42f3f44e9f27456bba5e
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-06-22 20:19:56 +0300

    Time: 103 ms (93.89%), Space: 17.8 MB (76.99%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+            def topological_sort(graph):
+                
+                in_degree = [0] * len(graph)
+                for node in range(len(graph)):
+                    for neighbor in graph[node]:
+                        in_degree[neighbor] += 1
+
+                queue = deque()
+                for node in range(len(graph)):
+                    if in_degree[node] == 0:
+                        queue.append(node)
+
+                result = []
+                while len(queue):
+                    node = queue.popleft()
+                    result.append(node)
+                    for neighbor in graph[node]:
+                        in_degree[neighbor] -= 1
+                        if in_degree[neighbor] == 0:
+                            queue.append(neighbor)
+
+               
+                if len(result) != len(graph):
+                    return False
+
+                return True
+            
+            graph=[[] for _ in range(numCourses)]
+            for u, v in prerequisites:
+                graph[v].append(u)
+            
+            return topological_sort(graph)
