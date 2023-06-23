706cdb44010504615ad815910f544bce6b4a1e1e
Robera16 <71070205+Robera16@users.noreply.github.com>
2023-06-23 23:50:05 +0300

    Time: 1859 ms (83.93%), Space: 109 MB (42.74%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,30 +1,21 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        # Store all edges in 'graph'.
         graph = collections.defaultdict(list)
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
         
-        print("graph", graph)
-
-        # Store all the nodes to be visited in 'queue'.
-        seen = [False] * n
-        seen[source] = True
-        queue = collections.deque([source])
+        visited = set([source])
+        queue = deque([source])
         
-        print(queue)
-        
-        while queue:
-            curr_node = queue.popleft()
-            if curr_node == destination:
+        while(len(queue)):
+            node = queue.popleft()
+            if node==destination:
                 return True
-
-            # For all the neighbors of the current node, if we haven't visit it before,
-            # add it to 'queue' and mark it as visited.
-            for next_node in graph[curr_node]:
-                if not seen[next_node]:
-                    seen[next_node] = True
-                    queue.append(next_node)
-        
+            
+            for neighbour in graph[node]:
+                if neighbour not in visited:
+                    visited.add(neighbour)
+                    queue.append(neighbour)
+                    
         return False
