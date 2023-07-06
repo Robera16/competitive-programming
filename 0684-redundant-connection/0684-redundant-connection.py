04fd8c77bf6783dbedf6d1b553e7169c82b8afad
Robera Workneh <71070205+Robera16@users.noreply.github.com>
2023-07-07 01:29:43 +0300

    Time: 67 ms (82.51%), Space: 17 MB (35.82%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,19 +1,31 @@
-class UnionFind:
-    def __init__(self, n):
-        self.root = [i for i in range(n)]
-    def find(self, x):
-        if x == self.root[x]: return x
-        return self.find(self.root[x])
-    def union(self, x, y):
-        X, Y = self.find(x), self.find(y)
-        if X != Y: self.root[X] = Y
-
-class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        uf = UnionFind(len(edges))
-        res = [0, 0]
-        for u, v in edges:
-            U, V = uf.find(u - 1), uf.find(v - 1)
-            if U == V: res = [u, v]
-            else: uf.union(u - 1, v - 1)
-        return res
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        n = len(edges)
+        par = [i for i in range(n+1)]
+        rank = [1]*(n+1)
+        
+        def find(n):
+            p = par[n]
+            while p!=par[p]:
+                par[p]=par[par[p]]
+                p = par[p]
+            return p
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            
+            if p1==p2:
+                return False
+            else:
+                if rank[p1] > rank[p2]:
+                    par[p2]=p1
+                    rank[p1]+=rank[p2]
+                else:
+                    par[p1]=p2
+                    rank[p2]+=rank[p1]
+                    
+                return True
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1,n2]
+        
