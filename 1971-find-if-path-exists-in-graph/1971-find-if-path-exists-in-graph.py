3b761b6ef2e4a15506d1ea6e3733515c144bfca6
Robera Workneh <71070205+Robera16@users.noreply.github.com>
2023-07-21 20:46:54 +0300

    Time: 2239 ms (35.69%), Space: 304.8 MB (28.80%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,21 +1,20 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-            graph = collections.defaultdict(list)
-            for u, v in edges:
-                graph[u].append(v)
-                graph[v].append(u)
-
-            visited = set([source])
-            queue = deque([source])
-
-            while(len(queue)):
-                node = queue.popleft()
-                if node==destination:
-                    return True
-
-                for neighbour in graph[node]:
-                    if neighbour not in visited:
-                        visited.add(neighbour)
-                        queue.append(neighbour)
-
-            return False
+        def dfs(vertex, graph, visited):
+            visited.add(vertex)
+            for neighbour in graph[vertex]:
+                if neighbour not in visited:
+                    dfs(neighbour,graph,visited)
+        
+        
+        graph = [[] for _ in range(n)] 
+        visited = set()
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        dfs(source, graph,visited)
+        
+        if destination in visited:
+            return True
+        return False
